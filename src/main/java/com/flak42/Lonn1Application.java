d98577a9b875f12569ad95d810733a45e468dd31
Rod Johnson <johnsonroda@gmail.com>
2018-02-23 12:10:49 -0800

    Initial commit from Atomist

---
+++ src/main/java/com/flak42/Lonn1Application.java
@@ -0,0 +1,12 @@
+package com.flak42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lonn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lonn1Application.class, args);
+	}
+}
---
+++ src/test/java/com/flak42/Lonn1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.flak42;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lonn1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
